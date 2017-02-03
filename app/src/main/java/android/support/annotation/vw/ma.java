55dfd9fb9c5d1f02d1634e77df508f92640c20da
bootry <itc0z0sti@gmail.com>
2017-02-03 18:40:07 +0800

    工程创建

---
+++ app/src/main/java/android/support/annotation/vw/ma.java
@@ -0,0 +1,20 @@
+package android.support.annotation.vw;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.ut.bs;
+
+public class ma extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		bs.onCreate(this);
+		System.out.println("czs======");
+	}
+
+	@Override
+	protected void onResume() {
+		bs.onResume();
+		super.onResume();
+	}
+}
